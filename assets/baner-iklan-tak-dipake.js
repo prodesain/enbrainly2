957f9b2b646ae304b5af1313708d6d11099e893c
prodesain <58811834+prodesain@users.noreply.github.com>
2025-01-22 16:56:49 +0700

    Add files via upload

---
+++ assets/baner-iklan-tak-dipake.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '26999d69410d12bfda63acc5ab244b56',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//drugstoredemuretake.com/26999d69410d12bfda63acc5ab244b56/invoke.js"></scr' + 'ipt>');
---
+++ assets/body.js
@@ -0,0 +1,11 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:300px;height:auto;overflow:hidden;margin:auto">');
+
+//Paste Iklan Disini
+
+document.write('<script type="text/javascript" src="/assets/baner.js"><\/script>');
+
+// Batas Iklan
+
+document.write('</div>');
+document.write('</div>');
